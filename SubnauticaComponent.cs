e193fa296ccf27e31fb14619dc6323feee3ef476
Meeperty <meepprograms@gmail.com>
2022-06-27 11:50:21 -0700

    basic component

---
+++ SubnauticaComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveSplit;
+using LiveSplit.UI.Components.AutoSplit;
+using LiveSplit.Model;
+using System.Xml;
+using LiveSplit.UI;
+using System.Windows.Forms;
+
+namespace SubnauticaAutosplitter
+{
+    public class SubnauticaComponent : AutoSplitComponent
+    {
+        internal SubnauticaComponent(LiveSplitState state) : base(new SubnauticaSplitter(settings), state) { }
+
+        private readonly static SubnauticaSettings settings = new SubnauticaSettings(); 
+
+        public override string ComponentName => "Subnautica Autosplitter";
+
+
+        public override void Dispose()
+        {
+        }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetSettings(XmlNode settings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SubnauticaSplitter.cs
@@ -0,0 +1,89 @@
+﻿using LiveSplit.ComponentUtil;
+using LiveSplit.Model;
+using LiveSplit.UI.Components.AutoSplit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubnauticaAutosplitter
+{
+    public class SubnauticaSplitter : IAutoSplitter
+    {
+        Process game;
+        Timer memCheckTimer;
+        bool lifepodCodeloaded = false;
+        bool launchRocketCodeLoaded = false;
+
+        private readonly SubnauticaSettings settings;
+        internal SubnauticaSplitter(SubnauticaSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        #region Memory & Such
+        public void GetGameProcess()
+        {
+            if (game == null)
+            {
+                game = Process.GetProcessesByName("Subnautica").FirstOrDefault(p => !p.HasExited);
+            }
+        }
+
+        public void CheckMemory(object o)
+        {
+            
+        }
+        
+        #endregion
+
+        #region Logic
+        public bool ShouldSplit(LiveSplitState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldReset(LiveSplitState state)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool ShouldStart(LiveSplitState state)
+        {
+            if (game == null)
+            {
+                GetGameProcess();
+                if (!(game == null))
+                {
+                    memCheckTimer = new Timer(CheckMemory, null, 0, 4000);
+                }
+                return false;
+            }
+            if (!lifepodCodeloaded)
+            {
+
+            }
+            return false;
+        }
+
+        public bool IsGameTimePaused(LiveSplitState state) { return false; }
+        public TimeSpan? GetGameTime(LiveSplitState state) { return null; }
+
+        #endregion Logic
+    }
+
+    internal class Signature
+    {
+        public string sig;
+        public int offset;
+
+        public Signature(string s, int o)
+        {
+            sig = s;
+            offset = o;
+        }
+    }
+}
